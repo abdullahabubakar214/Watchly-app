d7844ee613f57e5057b4d2d23d5e14e2383a1dc1
Abdullah Abu bakar <abdullahabubakar214@gmail.com>
2025-07-08 05:19:10 +0500

    remove bugs from user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -253,7 +253,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  return res.status(200, req.user, "Current user fetched Successfully");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched Successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -263,7 +265,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user =await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: { fullName, email: email },
